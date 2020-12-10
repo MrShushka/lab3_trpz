801c1e8abd0ee2db36838beb5fedafc90d7f5320
mrshushka <mrshushka99@gmail.com>
2020-12-10 15:40:17 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+
+    $(".zoom").hover(function(){
+
+        $(this).addClass('transition');
+    }, function(){
+
+        $(this).removeClass('transition');
+    });
+
+});
+$(function () {
+    $(".like").click(function () {
+        var input = $(this).find('.qty1');
+        input.val(parseInt(input.val())+ 1);
+    });
+
+    $(".dislike").click(function () {
+        var input = $(this).find('.qty2');
+        input.val(input.val() - 1);
+    });
+});
